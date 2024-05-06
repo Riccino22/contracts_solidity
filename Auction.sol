b41ee08c9252fed76254a09bb5e875c4c7b1a56a
Riccardo Inojosa <68616811+Riccino22@users.noreply.github.com>
2024-05-05 22:58:58 -0300

    Update Auction.sol

--- Auction.sol
+++ Auction.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 // Se especifica la licencia bajo la cual se distribuye el contrato
 
 pragma solidity 0.8.25;
@@ -7,24 +7,25 @@ pragma solidity 0.8.25;
 contract Subasta {
     // Se declara el contrato de subasta
 
-    enum EstadoSubasta {Activa, Vendida}
     // Se declara un tipo enumerado para el estado de la subasta, que puede ser "Activa" o "Vendida"
+    enum EstadoSubasta {Activa, Vendida}
 
+    // Se define una estructura para los elementos de la subasta
     struct ElementoSubasta {
-        // Se define una estructura para los elementos de la subasta
         string nombre; // Nombre del producto en subasta
         uint256 precioInicial; // Precio inicial del producto
         uint256 precioActual; // Precio actual del producto
         uint256 fechaInicio; // Fecha de inicio de la subasta (en marca de tiempo Unix)
         uint256 duracion; // Duración de la subasta en días
-        address propietario; // Dirección del propietario del producto
+        address payable creador; // Dirección del creador de la subasta
+        address payable propietario; // Dirección del propietario actual del producto
         EstadoSubasta estado; // Estado actual de la subasta
-        //mapping (address => uint256) ofertas; // Mapeo para almacenar ofertas de compradores
     }
 
-    mapping (address => ElementoSubasta[]) public subastas;
     // Se declara un mapeo que asocia direcciones con arrays de elementos de subasta
+    mapping (address => ElementoSubasta[]) public subastas;
 
+    // Eventos
     event NuevaSubasta(
         address vendedor, 
         uint256 indiceProducto, 
@@ -33,23 +34,23 @@ contract Subasta {
         uint256 fechaInicio, 
         uint256 duracion
     );
-    // Se declara un evento que se emite cuando se inicia una nueva subasta
-    event Oferta(address vendedor, uint256 indiceProducto, address comprador, uint256 cantidad);
+    event Oferta(bool secreta, address vendedor, uint256 indiceProducto, address comprador, uint256 cantidad);
     event SubastaFinalizada(address vendedor, string nombreProductoVendido, address nuevoPropietario, uint256 precio);
-    // Se declara un evento que se emite cuando una subasta finaliza y se vende un producto
+    event TransferenciaRealizada(address indexed comprador, address indexed vendedor, uint256 cantidad);
 
+    // Función para iniciar una nueva subasta
     function iniciarSubasta(string memory nombreProducto, uint256 precioInicial, uint256 duracion) public{
-        // Función para iniciar una nueva subasta
         ElementoSubasta memory miElementoSubasta = ElementoSubasta({
-            // Se crea un nuevo elemento de subasta
             nombre: nombreProducto,
             precioInicial: precioInicial,
             precioActual: precioInicial,
             fechaInicio: block.timestamp,
             duracion: duracion * 86400, // Se convierte la duración de días a segundos
-            propietario: msg.sender, // El iniciador de la subasta se convierte en el propietario
+            creador: payable(msg.sender), // El creador de la subasta es el iniciador
+            propietario: payable(msg.sender), // El iniciador también es el propietario inicial
             estado: EstadoSubasta.Activa // La subasta se marca como activa
         });
+
         subastas[msg.sender].push(miElementoSubasta); // Se agrega el elemento de subasta al array correspondiente
         emit NuevaSubasta(
             msg.sender,
@@ -61,17 +62,27 @@ contract Subasta {
         ); // Se emite el evento de nueva subasta
     }
 
-    function realizarOferta(address payable vendedor, uint256 indiceProducto, uint256 montoOferta) external payable {
-        // Función para realizar una oferta en una subasta existente
+    // Función para realizar una oferta en una subasta existente
+    function realizarOferta(bool secreta, address payable vendedor, uint256 indiceProducto, uint256 montoOferta) external payable {
         require(subastas[vendedor][indiceProducto].estado == EstadoSubasta.Activa, "Este producto ya ha sido vendido");
-        vendedor.transfer(msg.value);
-        emit Oferta(vendedor, indiceProducto, msg.sender, montoOferta);
-        subastas[vendedor][indiceProducto].propietario = msg.sender; // Se actualiza el propietario del elemento de subasta
+        require(montoOferta > subastas[vendedor][indiceProducto].precioActual, "La oferta no supera la mayor oferta");
+        require(msg.sender.balance >= montoOferta, "No tienes suficientes Ethers para realizar esta oferta");
+
+        emit Oferta(secreta, vendedor, indiceProducto, msg.sender, montoOferta);
+        subastas[vendedor][indiceProducto].propietario = payable(msg.sender); // Se actualiza el propietario del elemento de subasta
         subastas[vendedor][indiceProducto].precioActual = montoOferta; // Se actualiza el precio actual del elemento de subasta
     }
 
-    function finalizarSubasta(uint256 indiceProducto) public {
-        // Función para finalizar una subasta
+    // Función para finalizar una subasta
+    function finalizarSubasta(uint256 indiceProducto) external payable {
+        require(block.timestamp > subastas[msg.sender][indiceProducto].duracion + subastas[msg.sender][indiceProducto].fechaInicio, "El tiempo de la subasta aun no ha finalizado");
+        require(subastas[msg.sender][indiceProducto].propietario.balance >= subastas[msg.sender][indiceProducto].precioActual, "Error en la transaccion: Fondos insuficientes");
+        subastas[msg.sender][indiceProducto].creador.transfer(msg.value); // Se transfiere el monto a la cuenta del creador
+        emit TransferenciaRealizada(
+            subastas[msg.sender][indiceProducto].propietario, 
+            msg.sender,
+            subastas[msg.sender][indiceProducto].precioActual
+        );
         subastas[msg.sender][indiceProducto].estado = EstadoSubasta.Vendida; // Se marca la subasta como vendida
         emit SubastaFinalizada(
             msg.sender, 
@@ -79,6 +90,6 @@ contract Subasta {
             subastas[msg.sender][indiceProducto].propietario, 
             subastas[msg.sender][indiceProducto].precioActual
         ); // Se emite el evento de subasta finalizada
-    }
-}
 
+   }
+}
